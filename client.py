5a6041032694b715818f275d64226fef167f3fc7
AmirCM <amirbehzadfar.h@gmail.com>
2022-09-16 22:38:21 -0400

    Simple UDP connection server/ client

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+
+if __name__ == '__main__':
+    host_name = socket.gethostname()
+    server_port = 13333
+    outgoing_msg = sys.argv[1].encode()
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        client_socket.sendto(outgoing_msg, (host_name, server_port))
+        echo = client_socket.recv(1024)
+
+        print(f"ECHO from server => {echo.decode()}")
+
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+if __name__ == '__main__':
+    print("Server Running!!!")
+    my_host = ''
+    my_port = 13333
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.bind((my_host, my_port))
+
+        while True:
+            incoming, addr = server_socket.recvfrom(1024)
+            if not incoming:
+                break
+
+            server_socket.sendto(incoming, addr)
+            print(f"Incoming msg from {addr} connection, msg: {incoming.decode()}")
+
