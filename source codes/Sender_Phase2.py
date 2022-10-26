78fec3450c9714eca16af90294fee0bb97fccb4a
AmirCM <amirbehzadfar.h@gmail.com>
2022-10-26 12:24:36 -0400

    Checksum done!

--- source codes/Sender_Phase2.py
+++ source codes/Sender_Phase2.py
@@ -1,6 +1,20 @@
 from socket import *  # imports socket module to enable network communication
 
 
+def checksum(pkt):
+    ch = pkt[0:2]
+
+    for i in range(2, len(pkt), 2):
+        a = int.from_bytes(pkt[i:i+2], 'big') + int.from_bytes(ch, 'big')
+
+        if a > 65535:
+            a -= 65535
+
+        ch = (~a).to_bytes(3, 'big', signed=True)
+        ch = ch[1:]
+    return ch
+
+
 class Sender:
     def __init__(self, port, destination, sockets):
         self.port = port  # server port number
@@ -18,7 +32,7 @@ class Sender:
             i += 1  # increment loop variable
             self.packets.append(data)  # add packet to array
             if len(data) < 1024:  # once a packet is less than 1024 bytes, end of file reached, so break
-                image.close()     # close bmp file
+                image.close()  # close bmp file
                 break
 
     def socket_close(self):
@@ -31,5 +45,5 @@ if __name__ == '__main__':
         s.make_packet('../imgs/select_me.bmp')  # call function to parse bmp file into packets
 
         for packet in s.packets:  # loop through packet array and individually send to receiver
-            s.sockets.sendto(packet, (s.destination, s.port))
-
+            # s.sockets.sendto(packet, (s.destination, s.port))
+            print(checksum(packet))
