93f76ed44c0eacd1a32776ea6730265a81b58cb5
Kalvin McCallum <114109316+kalvinmccallum@users.noreply.github.com>
2022-10-29 13:46:57 -0400

    Update Receiver_Phase2.py

--- source codes/Receiver_Phase2.py
+++ source codes/Receiver_Phase2.py
@@ -1,101 +1,118 @@
+import socket
+import sys
 from socket import *  # imports socket module to enable network communication
 
 
-def extract_sender_data(data):
-    sender_packet_length = len(data)
-    return data[0: (sender_packet_length - 1)]
+ack = True
+states = ['w4zero', 'w4one']
+state = states[0]
+buffer = b''
+seqNum = False
+once_thru = 0
 
 
 class Receiver:
-    def __init__(self, port, sockets, data):
-        self.port = port  # Receiver port number
-        self.sockets = sockets  # Receiver socket
-        self.packets = []  # Receiver packets
-        self.socket_bind()  # Receiver bind to socket
-        self.data = data
+    def __init__(self, port, sockets):
+            self.port = port  # Receiver port number
+            self.sockets = sockets  # Receiver socket
+            self.packets = []  # Receiver packets
+            self.socket_bind()  # Receiver bind to socket
 
-    # Extract data from received sender packet
-    def extract_data(self, data):
-        sender_packet_length = len(data)
-        return data[0:(sender_packet_length - 5)]
 
-    # Function to generate checksum
-    def checksum(self, data):
+    def rdt_rcv(packet):# Function to receive packets from sender
+        if not packet:
+            return False
+        else:
+            return True
+
+    def checksum(data):# Function to generate checksum
         ch = data[0:2]
-
         for i in range(2, len(data), 2):
             a = int.from_bytes(data[i:i + 2], 'big') + int.from_bytes(ch, 'big')
-
             if a > 65535:
                 a -= 65535
-
             ch = (~a).to_bytes(3, 'big', signed=True)
             ch = ch[1:]
         return ch
 
-    # Function to check if the received packet has sequence number 0 or 1
-    def check_sequence_number(self, senderpacket):
-        if senderpacket.data[0:1] == '1':
-            return True
-        if senderpacket.data[0:1] == '0':
-            return False
 
-    # Function to check if sender packet is corrupt
-    def corrupt_check(self, senderpacket, checksum):
-        sender_packet_length = len(senderpacket)
-        if checksum(senderpacket[0:(sender_packet_length - 1)]) == senderpacket[
-                                                                   (sender_packet_length - 1):sender_packet_length]:
-            return True
+    def corrupt(rcv_pkt):# Function to check for corruption
+        pkt_len = len(rcv_pkt)
+        if r.checksum(rcv_pkt[0:(pkt_len - 1)]) == rcv_pkt[(pkt_len - 1):pkt_len]:
+            return False
         else:
+            return True
+
+    def get_seq(rcv_pkt):# Function to extract sequence number from received packet
+        if rcv_pkt[0:1] == '1':
+            return True
+        if rcv_pkt[0:1] == '0':
             return False
 
-    # Function used to make image file for Phase 2
-    def make_file(self, path):
-        with open(path, 'wb') as image:
-            for i, p in enumerate(self.packets):
-                skip = i * 1024  # skip variable holds number of bytes already stored
-                image.seek(skip)  # skip over bytes already stored as packets
-                image.write(p)  # writing packets to file
+    def extract(packet):# Function to extract data from received packet
+        received_packet_length = len(packet)
+        return packet[0:(received_packet_length - 5)]
 
-    # Function to make packet that will be returned to sender
-    def make_packet(self, data, checksum):
-        i = 0
-        sequence_num = i % 2
-        chunk = self.data.read(1024)
-        chunk += sequence_num.to_bytes(1, 'big')
-        receiverpacket = str(int(sequence_num)).encode() + data + checksum
-        return receiverpacket
+    def deliver_data(data):# Function to deliver data
+        global buffer
+        buffer = buffer + data
+        return
 
-    # Function to initialize receiver and begin listening
-    def start(self):
+    def make_pkt(seqNum, ack, checksum):# Function to make packet that will be delivered to sender
+        packet = str(int(seqNum)).encode + ack + checksum
+        return packet
+
+    def udt_send(packet): # Function to send packet to sender
+        sender_socket = socket(AF_INET, SOCK_DGRAM)
+        sender_socket.sendto(packet, (sender.destination, sender.port))
+        sender_socket.close()
+        return
+
+    def start(self): # Function to start
         self.packets = []
         while True:
             incoming, address = self.sockets.recvfrom(1024)
             self.packets.append(incoming)  # packet to array
-            print(len(incoming))
             if len(incoming) < 1024:
                 break
 
-    def socket_bind(self):
-        self.sockets.bind(('', self.port))  # binds to the socket
+    def socket_bind(self): # Function to bind socket
+            self.sockets.bind(('', self.port))  # binds to the socket
 
+if __name__ == '__main__': # Main function begins here
+        r = Receiver(12000, socket(AF_INET, SOCK_DGRAM))
+        r.start()
+        recv_pkt = r.sockets.recvfrom(1024)
+        data = r.extract(recv_pkt)
 
-if __name__ == '__main__':
-    r = Receiver(12000, socket(AF_INET, SOCK_DGRAM))  # create instance of Receiver class
-    r.start()  # Start server & listening for incoming data
+        if state == states[0]: # Conditional statement for first state
 
-    # State 1
-    # If the received packet is corrupt or sequence with ‘1’ it sends acknowledgment with sequence number ‘1’ to it which tells the sender that the packet sent was not in order.
-    if corrupt_check(senderpacket) or check_sequence_number(senderpacket) == 'True':
+            if r.rdt_rcv(recv_pkt) and (r.corrupt(recv_pkt) or r.get_seq(recv_pkt) == 1):
+                if once_thru == 1
+                    checksum = r.checksum(seqNum, data)
+                    send_pkt = r.make_pkt(1, ack, checksum)
+                    r.udt_send(send_pkt)
 
-    # If the packet is not corrupt and has sequence number ‘0’ the receiver extracts the data and sends acknowledgment with the sequence ‘0’. It moves to the next state.
-    if not corrupt_check(senderpacket) and check_sequence_number(senderpacket) == 'False':
+            elif r.rdt_rcv(recv_pkt) and (not r.corrupt(recv_pkt)) and r.get_seq(recv_pkt) == 0:
+                r.extract(recv_pkt, data)
+                r.deliver_data(data)
+                checksum = r.checksum(seqNum, data)
+                send_pkt = r.make_pkt(0, ack, checksum)
+                r.udt_send(send_pkt)
+                once_thru = 1
+                state = states[1]  # Next State
 
-    # State 2
-    # If the received packet is corrupt or sequence with ‘0’ it sends acknowledgment with sequence number ‘0’ to it which tells the sender that the packet sent was not in order.
-    if corrupt_check(senderpacket) or check_sequence_number(senderpacket) == 'False':
+        elif states == states[1]: # Conditional statement for second state
 
-    # If the packet is not corrupt and has sequence number ‘1’ the receiver extracts the data and sends acknowledgment with the sequence ‘1’. It moves to the next state.
-    if not corrupt_check(senderpacket) and check_sequence_number(senderpacket) == 'True':
+            if r.rdt_rcv(recv_pkt) and (r.corrupt(recv_pkt) or r.get_seq(recv_pkt) == 0):
+                checksum = r.checksum(seqNum, data)
+                send_pkt = r.make_pkt(0, ack, checksum)
+                r.udt_send(send_pkt)
 
-    r.make_file('../imgs/received_image.bmp')  # rebuild & save the image
+            elif r.rdt_rcv(recv_pkt) and (not r.corrupt(recv_pkt)) and r.get_seq(recv_pkt) == 1:
+                r.extract(recv_pkt)
+                r.deliver_data(data)
+                checksum = r.checksum(seqNum, data)
+                send_pkt = r.make_pkt(1, ack, checksum)
+                r.udt_send(send_pkt)
+                state = states[0]  # Next State
