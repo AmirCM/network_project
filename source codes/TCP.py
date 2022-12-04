1caf4e84a2da07e97877424d4d36400eb16bdf0e
AmirCM <amirbehzadfar.h@gmail.com>
2022-12-04 16:57:54 -0500

    TCP class

---
+++ source codes/TCP.py
@@ -0,0 +1,24 @@
+import time
+from socket import *  # imports socket module to enable network communication
+import numpy as np
+
+
+class TCP:
+    def __init__(self):
+        self.MSS = 1000
+        self.segment = {'source_port': 0,  # 2B
+                        'dest_port': 0,  # 2B
+                        'seq_num': 0,  # 4B counting bytes of data
+                        'ack_num': 0,  # Next expected B
+                        'head_len': 0,  # 1B
+                        'C': b'0',  # 1 bit congestion
+                        'E': b'0',  # 1 bit congestion
+                        'U': b'0',  # 1 bit
+                        'A': b'0',  # 1 bit Ack
+                        'P': b'0',  # 1 bit
+                        'R': b'0',  # 1 bit flow
+                        'S': b'0',  # 1 bit flow
+                        'F': b'0',  # 1 bit flow
+                        'rec_window': 0,  # 2B rec window remaining size
+                        'checksum': 0,  # 2B
+                        }
