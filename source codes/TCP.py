09148589ddd5282ff07e515f6b01a25cf647d9df
AmirCM <amirbehzadfar.h@gmail.com>
2022-12-07 18:16:25 -0500

    TCP class

--- source codes/TCP.py
+++ source codes/TCP.py
@@ -1,11 +1,15 @@
+import socket
 import time
 from socket import *  # imports socket module to enable network communication
 import numpy as np
-
+from typing import Tuple
 
 class TCP:
-    def __init__(self):
+    def __init__(self, SOCKET: socket):
         self.MSS = 1000
+        self.s = SOCKET
+        self.recv_pkt = None
+        self.dst_addr = None
         self.segment = {'source_port': 0,  # 2B
                         'dest_port': 0,  # 2B
                         'seq_num': 0,  # 4B counting bytes of data
@@ -23,10 +27,34 @@ class TCP:
                         'checksum': 0,  # 2B
                         }
 
-rtt = 30 / 1000
-alpha = 0.25
-for _ in range(50):
-    stamp = time.time()
-    time.sleep(0.6)
-    rtt = rtt*(1-alpha) +  (time.time() - stamp)*alpha
-    print(rtt, (time.time() - stamp))
+    def bind(self, HOST, PORT):
+        self.s.bind((HOST, PORT))
+
+    def listen(self):
+        self.recv_pkt, self.dst_addr = self.s.recvfrom(1024)
+
+    def tcp_recv(self):
+        # Check checksum
+        # Handle seq
+        return self.s.recv(1024)
+
+    def tcp_send(self):
+        return self.s.sendto(data, (gethostname(), 12000))
+
+    def accept(self) -> Tuple[socket, tuple]:
+        if self.s.connect(self.dst_addr) == 0:
+            return self.s, self.dst_addr
+        return 0, 0
+
+
+if __name__ == '__main__':
+    with socket(AF_INET, SOCK_DGRAM) as client_socket:
+        server = TCP(client_socket)
+        server.bind('', 12000)
+        server.listen()
+        conn, addr = server.accept()
+        while True:
+            data =  conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
