cf251bd8655c30457390495937ce5581c84f0155
Abhi Patel <abhipatel499@gmail.com>
2022-12-08 00:01:13 -0500

    updates

--- source codes/TCP_Receiver.py
+++ source codes/TCP_Receiver.py
@@ -8,9 +8,9 @@ from TCP import *
 class Receiver:
     def __init__(self, port):
         self.sockets = socket(AF_INET, SOCK_DGRAM)  # Receiver socket
-        self.sockets.bind(('', port))  # Receiver socket bind
         self.dst_addr = None
         self.recv_pkt = None
+        self.port = port
 
     def rdt_rcv(self) -> bool:
         self.recv_pkt, self.dst_addr = self.sockets.recvfrom(pkt_len)
@@ -35,11 +35,33 @@ class Receiver:
         seq_num = seq_num.to_bytes(2, 'big')
         return seq_num + checksum(seq_num)
 
+    def listen(self):
+        while True:
+            incoming, dst_addr = self.sockets.recvfrom(1024)
+            if not corrupt(incoming):
+                break
+        if check_flag_sync(incoming):
+            seq_num = get_seqNum(incoming)
+            packet = Segment()
+            packet.header['ack_num'] = seq_num + 1
+            packet.header['seq_num'] = 10
+            packet.flags['A'] = 0b1
+            packet.flags['S'] = 0b1
+            pkt = packet.make_packet(''.encode())
+            self.sockets.sendto(pkt, dst_addr)
+        else:
+            return False
+
+        while True:
+            incoming, dst_addr = sockets.recvfrom(1024)
+            if not corrupt(incoming):
+                break
+        if check_flag_ack(incoming) and get_ack_num(incoming) == 11:
+            print('Connection successful')
+            return True
+
 
 if __name__ == '__main__':
-    with socket(AF_INET, SOCK_DGRAM) as server_socket:
-        server = TCP(server_socket)
-        server.bind('', 12000)
-        print(server.dst_addr)
-        if server.listen():
-            print('Handshake complete')
+        r = Receiver(12000)
+        r.sockets.bind(('', 12000))
+        r.listen()
